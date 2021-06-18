224f6f5454a24de331d849db3a1d9e695806b94f
Fahadstudyy <fahadstudyy@gmail.com>
2021-06-18 17:40:45 +0500

    Url file update

--- TodoWoo/urls.py
+++ TodoWoo/urls.py
@@ -35,5 +35,6 @@ urlpatterns = [
     path('todo/<int:todo_pk>',views.viewtodo, name='viewtodo'),
     path('todo/<int:todo_pk>/complete',views.completetodo, name='completetodo'),
     path('todo/<int:todo_pk>/delete',views.deletetodo, name='deletetodo'),
+]
 
 
